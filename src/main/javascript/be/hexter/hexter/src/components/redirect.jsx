61f11c9269e190bbfe8a1a8fafdb743c7cb8faee
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-11-13 11:05:32 +0100

    Created an redirect component.

---
+++ src/main/javascript/be/hexter/hexter/src/components/redirect.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { push } from "redux-first-routing";
+import { useNavigate } from "react-router-dom";
+
+const mapStateToProps = ({ state }) => ({
+  updateTick: state.updateTick,
+});
+const mapDispatchToProps = (dispatch) => ({
+  changePath: (path) => dispatch(push(path)),
+});
+
+function Redirect({ updateTick, path, changePath }) {
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    changePath(path);
+  }, [updateTick]);
+  navigateTo(path);
+
+  return <>redirecting..</>;
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Redirect);
