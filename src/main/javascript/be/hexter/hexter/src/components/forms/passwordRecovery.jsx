c892a8f4ce3074b761d802731192ba1d4fcb627b
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-11-13 18:36:28 +0100

    Working on password recovery form.

--- src/main/javascript/be/hexter/hexter/src/components/forms/passwordRecovery.jsx
+++ src/main/javascript/be/hexter/hexter/src/components/forms/passwordRecovery.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+import "./passwordRecovery.scss";
 
 function PasswordRecovery() {
-  return <>recover</>;
+  return (
+    <Form className="password-recovery-form">
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Forgot your password?</Form.Label>
+        <Form.Control />
+      </Form.Group>
+    </Form>
+  );
 }
 
 export default PasswordRecovery;
