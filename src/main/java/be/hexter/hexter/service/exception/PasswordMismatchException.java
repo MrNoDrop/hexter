f262562c5c98f0ab0158dcdef6bdf39916b03734
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-11-14 10:20:11 +0100

    Removed unnecesary info from message string.

--- src/main/java/be/hexter/hexter/service/exception/PasswordMismatchException.java
+++ src/main/java/be/hexter/hexter/service/exception/PasswordMismatchException.java
@@ -3,6 +3,6 @@ package be.hexter.hexter.service.exception;
 public class PasswordMismatchException extends RuntimeException {
 
     public PasswordMismatchException(String email) {
-        super("[PASSWORD]:The given password doesn't correspond to \"" + email + "\".");
+        super("[PASSWORD]:The given password doesn't correspond.");
     }
 }
