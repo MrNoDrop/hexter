56f1b6a912b9d539828b68269d72510460f969be
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-11-14 10:08:26 +0100

    Added email formatting to exception.

--- src/main/java/be/hexter/hexter/service/exception/EmailUnregisteredException.java
+++ src/main/java/be/hexter/hexter/service/exception/EmailUnregisteredException.java
@@ -3,6 +3,6 @@ package be.hexter.hexter.service.exception;
 public class EmailUnregisteredException extends RuntimeException {
 
     public EmailUnregisteredException(String email) {
-        super("[EMAIL]:The email \"" + email + "\" is unregistered.");
+        super("[EMAIL]:The email \"" + email.substring(10, email.length() - 2) + "\" is unregistered.");
     }
 }
