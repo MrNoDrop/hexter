305f3cb21b18d496b1ef97d33a15aa49311de984
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2026-03-06 15:08:41 +0100

    fixed wweb security config to original.

--- src/main/java/be/hexter/hexter/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/be/hexter/hexter/configuration/security/WebSecurityConfiguration.java
@@ -1,18 +1,21 @@
 package be.hexter.hexter.configuration.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile("!test & !dev")
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable();
+        // .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        return http.build();
     }
 }
